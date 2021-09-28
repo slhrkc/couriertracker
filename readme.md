88eead72760133293e0de5916dbac03b64c699ea
Salih Erikci <salih.erikci@turkcell.com.tr>
2021-09-28 18:30:18 +0300

    Updates

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Getting Started
+
+### Reference Documentation
+To run the application use below commands in the main directory of the project
+
+mvn clean install
+java -jar target/couriertracker-0.0.1-SNAPSHOT.jar
+
+It will be loaded on http://localhost:8080
+
+To test the API
--- src/main/java/com/salih/migros/couriertracker/CouriertrackerApplication.java
+++ src/main/java/com/salih/migros/couriertracker/CouriertrackerApplication.java
@@ -2,8 +2,10 @@ package com.salih.migros.couriertracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CouriertrackerApplication {
 
 	public static void main(String[] args) {
