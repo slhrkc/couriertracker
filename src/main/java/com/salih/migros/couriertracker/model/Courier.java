152b037375e4fe5f943bb460e6cacf4d3f207b4d
Salih Erikci <salih.erikci@turkcell.com.tr>
2021-09-28 18:31:33 +0300

    Update Courier.java

--- src/main/java/com/salih/migros/couriertracker/model/Courier.java
+++ src/main/java/com/salih/migros/couriertracker/model/Courier.java
@@ -2,7 +2,6 @@ package com.salih.migros.couriertracker.model;
 
 public class Courier {
     private Long courierId;
-    private String courierName;
 
     public Long getCourierId() {
         return courierId;
@@ -10,13 +9,5 @@ public class Courier {
 
     public void setCourierId(Long courierId) {
         this.courierId = courierId;
-    }
-
-    public String getCourierName() {
-        return courierName;
-    }
-
-    public void setCourierName(String courierName) {
-        this.courierName = courierName;
     }
 }
